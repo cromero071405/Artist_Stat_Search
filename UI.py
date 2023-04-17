04043aabde04ff7d04c1850ab8ff05b70d14c18c
cromero071405 <cromero5503@gmail.com>
2023-04-17 16:37:29 -0700

    Configuring Tkinter Window Background
    I changed the background of the Tkinter window from black to white.

--- UI.py
+++ UI.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 window = tk.Tk(className=' Artist Stat Search ') # # window is an instance of the Tk (Tkinter) class
 window.geometry('1350x720')  # To set a specific size of a Tkinter window.
-
+window.configure(bg='white') # To set background of Tkinter window to the color white.
 
 
 
