33a5356fffa4936e65e1c959bec446b455e858c5
cromero071405 <cromero5503@gmail.com>
2023-04-17 16:34:02 -0700

    Initial commit

---
+++ UI.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+window = tk.Tk(className=' Artist Stat Search ') # # window is an instance of the Tk (Tkinter) class
+window.geometry('1350x720')  # To set a specific size of a Tkinter window.
+
+
+
+
+
+
+
+# If application doesn't pop up, it is because we need the ".mainloop()"
+window.mainloop()
